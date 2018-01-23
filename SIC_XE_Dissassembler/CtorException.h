42521efd46f51a6dd50de7ae6f07090cdde9f132
Ethan <ethcf@yahoo.com>
2018-01-23 08:08:16 -0800

    removed CtorException since its no longer needed

--- SIC_XE_Dissassembler/CtorException.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-//http://www.cplusplus.com/doc/tutorial/exceptions/
-class CtorException {
-	virtual const char* what() const throw() {
-		return "Class DEFAULT constructor was called";
-	};
-} defaultCtorExcept;
